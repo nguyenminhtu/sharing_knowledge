455eb23199246dc00397a55e1a3f338225709859
Cuong Tran Duc <trancuong200495@gmail.com>
2018-04-14 22:39:41 +0700

    complete setup client

--- client/src/index.js
+++ client/src/index.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 // main app
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App/>, document.getElementById('app'))
