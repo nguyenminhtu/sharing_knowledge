021d4e0905a3d3ba2de60847ec20f70daeb04878
Cuong Tran Duc <trancuong200495@gmail.com>
2018-04-14 20:10:55 +0700

    config webpack

---
+++ client/webpack.dev.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var parentDir = path.join(__dirname, '/');
+
+module.exports = {
+    entry: "./src/index.js",
+    module: {
+        loaders: [{
+            test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },{
+                test: /\.less$/,
+                loaders: ["style-loader", "css-loder", "less-loader"]
+            }
+        ]
+    },
+    output: {
+        path: parentDir + '/dist',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: parentDir,
+        historyApiFallback: true
+    }
+}
